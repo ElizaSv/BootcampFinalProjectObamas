f3287cbc07a28c0ceaa0bf174d9ec00ce839c824
Niseme <nijolesemele@gmail.com>
2022-09-20 15:46:28 +0300

    authentication init commit

--- src/components/register/Auth.jsx
+++ src/components/register/Auth.jsx
@@ -1,24 +1,41 @@
+import React from "react";
+import axios from "axios";
 import "./auth.css";
 
-const Auth = ({ userName, password, email, buttonText, isRegister }) => {
+
+const Auth = () => {
+const[formData,setFormData] = React.useState({
+  userName:"",
+  password:"",
+  email:""
+})
+
+
+const handleChange =(e)=>{
+  setFormData({...formData,[e.target.name]:e.target.value})
+}
+
+
+  const submitForm = async() => {
+    const data = await axios.post("http://localhost:8000/auth/registration",{...formData})
+    console.log(data);
+  }
+
+
   return (
     <div className="auth">
       <div className="authWrapper">
         <div className="authMain">
           <div className="authInput">
-            <label>{userName}</label>
-            <input type="text" placeholder={userName} />
-            <label>{password}</label>
-            <input type="password" placeholder={password} />
-            {isRegister && (
-              <>
-                <label>{email}</label>
-                <input type="email" placeholder={email} />
-              </>
-            )}
+            <label>{'userName'}</label>
+            <input onChange={handleChange} type="text" name="userName" placeholder="userName" />
+            <label>{'password'}</label>
+            <input onChange={handleChange} type="password" name="password" placeholder={'password'} />
+                <label>{'email'}</label>
+                <input onChange={handleChange} type="email" name="email" placeholder={'email'} />
           </div>
           <div className="authButton">
-            <button>{buttonText}</button>
+            <button onClick={submitForm} >Submit.</button>
           </div>
         </div>
       </div>
