54a8faafb57a4b88a842d7375de7f43a6d7c6238
Niseme <nijolesemele@gmail.com>
2022-09-22 18:55:15 +0300

    map update

--- src/components/MapSection/BasicMap.jsx
+++ src/components/MapSection/BasicMap.jsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import "./basicMap.css";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css"
+import axios from "axios";
 
 const markerIcon = new L.Icon({
     iconUrl: require("./mapmarker.png"),
@@ -13,8 +14,27 @@ const markerIcon = new L.Icon({
 
 const BasicMap = () => {
   const [center, setCenter] = useState({ lat: 51.505, lng: 30.258 });
+  const[data,setData] = useState([]);
   const ZOOM_LEVEL = 9;
   const mapRef = useRef();
+
+// data fetching
+    
+React.useEffect(()=>{
+  axios("http://localhost:8000/events").then(data=>{
+    setData(data.data)
+  }).catch((err)=>{
+    console.log(err)
+  });
+
+},[])
+
+
+if(data.length === 0){
+  return "Loading...."
+}
+
+
   return (
     <div className="row">
       <div className="col text-center">
@@ -22,11 +42,18 @@ const BasicMap = () => {
           <MapContainer  center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
             <TileLayer url={osm.maptiler.url}>
             </TileLayer>
-            <Marker position={center} icon={markerIcon} > 
-                <Popup>
-                    <b>first popup</b>
-                 </Popup> 
-            </Marker>
+            {
+              data.map(val=>{
+                return(
+                  <Marker position={[val.lat?val.lat:20,val.lng?val.lng:30]} icon={markerIcon} > 
+                  <Popup>
+                      <b>{val.event?val.event:"happy"}</b>
+                   </Popup> 
+              </Marker>
+                );
+              })
+            }
+          
           </MapContainer >
           </div>
         </div>
