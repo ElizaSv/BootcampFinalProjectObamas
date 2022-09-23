582ce23e45e533e8f0b1313c9b32e11580c46457
Elis Pelis <eliza.svampe@gmail.com>
2022-09-23 11:55:36 +0200

    adjusting package files

--- src/components/TopSection/TopSection.jsx
+++ src/components/TopSection/TopSection.jsx
@@ -1,12 +1,38 @@
-import './TopSection.css'
+import "./TopSection.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faArrowRightToBracket,
+  faRegistered,
+  faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
 
 const TopSection = () => {
   return (
     <div className="topSection">
-        <h1 className="title">The magical life of Barack and Michelle Obama</h1>
-        <h4 className="titleInfo">One of the most powerful couples in the history. Go trough their separate and common journeys... Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi, harum. </h4>
+      <h1 className="title">The magical life of Barack and Michelle Obama</h1>
+      <h4 className="titleInfo">
+        One of the most powerful couples in the history. Go trough their
+        separate and common journeys... Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Modi, harum.
+      </h4>
+      <div className="links">
+        <Link to="/">
+          <FontAwesomeIcon icon={faHouse} className="link-icon" />
+        </Link>
+        <Link to="/map">
+          <FontAwesomeIcon icon={faGlobe}  className="link-icon"/>
+        </Link>
+        <Link to="/login">
+          <FontAwesomeIcon icon={faArrowRightToBracket}  className="link-icon"/>
+        </Link>
+        <Link to="/register">
+          <FontAwesomeIcon icon={faRegistered}  className="link-icon"/>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopSection
+export default TopSection;
