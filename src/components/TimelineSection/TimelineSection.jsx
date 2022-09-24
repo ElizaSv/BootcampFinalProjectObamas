21aecb8e740b8950f4e00efed574ccaece5e3513
Niseme <nijolesemele@gmail.com>
2022-09-24 12:51:58 +0300

    like dislike buttons

--- src/components/TimelineSection/TimelineSection.jsx
+++ src/components/TimelineSection/TimelineSection.jsx
@@ -6,30 +6,53 @@ import { Context } from "../../Context";
 //import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const TimelineSection = (props) => {
-  let {user} = useContext(Context)
+  let { user } = useContext(Context);
 
-  function likeHandler(){
+  function likeHandler() {
     //makerequest find your like and change it to positive or add it if none
+   props.handleLike(true)
   }
-  function dislikeHandler(){
+  function dislikeHandler() {
+    props.handleLike(false)
   }
+
   return (
-      <VerticalTimelineElement className="vertical-timeline-element--work">
-        <img src={props.image} className="timeline-img" alt="history"></img>
-        <h3 className="vertical-timeline-element-title">{props.year} {props.month}</h3>
-        <h4 className="vertical-timeline-element-subtitle">{props.event}</h4>
-        <p>{props.notes}</p>
-        <button className="event-btn" onClick={() => window.open(props.more, "_blank")}>Read more</button>
-        
-        {user && (
-          <>
-            <button className="like-btn" disabled={props.youLiked} style={{ background: props.youLiked ? "blue" : "white" }}>Like {props.likes>0? props.likes : null }</button>
-            <button className="like-btn" disabled={props.youDisliked } style={{ background: props.youDisliked ? "red" : "white" }}>Dislike {props.dislikes>0? props.dislikes : null}</button>
-          </>
-        )}
-      </VerticalTimelineElement>
+    <VerticalTimelineElement className="vertical-timeline-element--work">
+      <img src={props.image} className="timeline-img" alt="history"></img>
+      <h3 className="vertical-timeline-element-title">
+        {props.year} {props.month}
+      </h3>
+      <h4 className="vertical-timeline-element-subtitle">{props.event}</h4>
+      <p>{props.notes}</p>
+      <button
+        className="event-btn"
+        onClick={() => window.open(props.more, "_blank")}
+      >
+        Read more
+      </button>
+
+      {user && (
+        <>
+          <button
+            className="like-btn"
+            onClick={likeHandler}
+            disabled={props.youLiked}
+            style={{ background: props.youLiked ? "blue" : "white" }}
+          >
+            Like {props.likes > 0 ? props.likes : null}
+          </button>
+          <button
+            className="like-btn"
+            onClick={dislikeHandler}
+            disabled={props.youDisliked}
+            style={{ background: props.youDisliked ? "red" : "white" }}
+          >
+            Dislike {props.dislikes > 0 ? props.dislikes : null}
+          </button>
+        </>
+      )}
+    </VerticalTimelineElement>
   );
 };
-
 
 export default TimelineSection;
--- src/pages/frontPage/FrontPage.jsx
+++ src/pages/frontPage/FrontPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect,useMemo } from "react";
 import "./frontPage.css";
 import likes from '../../likes.json'
 import TimelineSection from "../../components/TimelineSection/TimelineSection";
@@ -9,57 +9,70 @@ import axios from 'axios'
 import NavIcons from "../../components/NavIcons/NavIcons";
 
 const FrontPage = () => {
-  let {user} = useContext(Context);
-  console.log(user)
-  let likeAggragation = likes.reduce((obj,i)=>{
-    if(!(i.postId in obj)){
-      obj[i.postId]={
-        likes:0,
-        dislikes:0,
-        youLiked:0,
-        youDisliked:0
-      }
-    }
-    if(i.mark){
-      obj[i.postId].likes++
-      if(user && i.personId===user._id){
-        obj[i.postId].youLiked++;
-      }
-    }else{
-      obj[i.postId].dislikes++;
-      if (user && i.personId === user._id) {
-        obj[i.postId].youDisliked++;
-      }
-    }
-    return obj
-  },{})
-  console.log(likeAggragation)
-
-// --- GETTING TIMELINE DATA FROM DATABASE ---//
+  const {user} = useContext(Context);
+  const [likeState, setLikeState]=useState(likes)
   const [timelineEvents, setTimelineEvents] = useState([])
 
+  function handleLike(postId,value) {
+    let index = likeState.findIndex(like=>like.postId === postId)
+    if(index >= 0){
+      likeState[index].mark = value
+    }else{
+      likeState.push({personId:user._id,postId:postId, mark:value})
+    }
+  
+    setLikeState([...likeState])
+    console.log(likeState)
+  }
+
+  // console.log(user)
+  let likeAggragation = useMemo(
+    ()=>likeState.reduce((obj, i) => {
+      if (!(i.postId in obj)) {
+        obj[i.postId] = {
+          likes: 0,
+          dislikes: 0,
+          youLiked: 0,
+          youDisliked: 0,
+        };
+      }
+      if (i.mark) {
+        obj[i.postId].likes++;
+        if (user && i.personId === user._id) {
+          obj[i.postId].youLiked++;
+        }
+      } else {
+        obj[i.postId].dislikes++;
+        if (user && i.personId === user._id) {
+          obj[i.postId].youDisliked++;
+        }
+      }
+      return obj;
+    }, {}),
+    [likeState]
+  );
+  // console.log(likeAggragation)
+
+// --- GETTING TIMELINE DATA FROM DATABASE ---//
+
   useEffect(() => {
-  axios.get('http://localhost:8000/events').then(result => setTimelineEvents(result.data))  
+    axios.get('http://localhost:8000/events').then(result => setTimelineEvents(result.data))  
   }, [])
 
   const sortedTimeline = timelineEvents
-    .sort((a, b) => b.year - a.year)
-    .map((i, index) => ({ ...i, ...(likeAggragation[index]||{likes:0,dislikes:0}) })); //This should later go in useState()
+    .sort((a, b) => b.year - a.year); //This should later go in useState()
 
   return (
-    <> 
-    <NavIcons />
-    <TopSection />
-    <VerticalTimeline>
-      {sortedTimeline.map((elem,index) => {
-        return (
-          <TimelineSection
-          key={index}
-            {...elem}
-          />
-        );
-      })}
-    </VerticalTimeline>
+    <>
+      <NavIcons />
+      <TopSection />
+      <VerticalTimeline>
+        {sortedTimeline.map((i, index) => ({ ...i, ...(likeAggragation[index]||{likes:0,dislikes:0}) })).map((elem, index) => {
+          return (
+            <TimelineSection handleLike={(value)=>handleLike(index,value)} key={index} {...elem} />
+          );
+        })}
+      </VerticalTimeline>
     </>
   );
 };
