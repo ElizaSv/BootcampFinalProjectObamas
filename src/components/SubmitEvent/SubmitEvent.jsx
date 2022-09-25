eae6219fdde6cc4dea90db3a35896cce7cac5625
Elis Pelis <eliza.svampe@gmail.com>
2022-09-25 17:44:14 +0200

    adding photo upload #2

--- src/components/SubmitEvent/SubmitEvent.jsx
+++ src/components/SubmitEvent/SubmitEvent.jsx
@@ -4,20 +4,33 @@ import { useForm } from "react-hook-form";
 
 const SubmitEvent = () => {
  
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = formData =>
-        {
-        console.log("--------------------------------------");
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+    const onSubmit = async formData => {
+        const convertToBase64 = file => new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = error => reject(error);
+        });
+        const getURL = async () => {
+            try {
+                return await convertToBase64(formData.image[0])
+            } catch (error) {
+                console.error(error)
+            }
+        }
+                     
         const createdEvent = JSON.stringify(formData);
-        console.log(createdEvent)
-        axios
-        .post('http://localhost:8000/events/new', createdEvent ,{headers: {'Content-Type': 'application/json'}})
-        .then(response => console.log(response.data))
-        .catch(error => {console.log(error.data)});
-        } 
+        const normalObj = JSON.parse(createdEvent)
+        normalObj.image = await getURL()
+        const resultingEvent = JSON.stringify(normalObj)
 
- 
-    // console.log(watch("event"));
+        axios
+        .post('http://localhost:8000/events/new', resultingEvent, {headers: {'Content-Type': 'application/json'}})
+        .then(response => console.log(response.data))
+        .catch(error => {console.log(error.data)});}
+
     return (
         <div className='submit-wrapper'>
             <div className='content-left'>
@@ -30,20 +43,20 @@ const SubmitEvent = () => {
             <div className='content-right'>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor="event">What was the event?</label>
-                    <input type="text" placeholder=" e.g. Barack Obama in local charity party" name="event" {...register("event", { required: true })}></input>
-                    {errors.event && <span className="error-msg">This field is required</span>}<br />
+                        <input type="text" placeholder=" e.g. Barack Obama in local charity party" name="event" {...register("event", { required: true })}></input>
+                            {errors.event && <span className="error-msg">This field is required</span>}<br />
                     <label htmlFor="year">When the event took place?</label>
-                    <input type="number" placeholder=" enter year (e.g. 2009)" name="year" {...register("year", { required: true })}></input>
-                    {errors.year && <span className="error-msg">This field is required</span>}<br />
+                        <input type="number" placeholder=" enter year (e.g. 2009)" name="year" {...register("year", { required: true })}></input>
+                            {errors.year && <span className="error-msg">This field is required</span>}<br />
                     <label htmlFor="month"> What month was that?</label>
-                    <input type="text" placeholder=" e.g. February" name="month" {...register("month", { required: true })}></input>
-                    {errors.month && <span className="error-msg">This field is required</span>}<br />
+                        <input type="text" placeholder=" e.g. February" name="month" {...register("month", { required: true })}></input>
+                            {errors.month && <span className="error-msg">This field is required</span>}<br />
                     <label htmlFor="notes">Please briefly describe what happened in event!</label>
-                    <input type="text" placeholder=" write your story here..." name="notes" {...register("notes", { required: true })}></input>
-                    {errors.notes && <span className="error-msg">This field is required</span>}<br />
+                        <input type="text" placeholder=" write your story here..." name="notes" {...register("notes", { required: true })}></input>
+                            {errors.notes && <span className="error-msg">This field is required</span>}<br />
                     <label> Do you have an image from that event? </label>
-                    <input type="file" accept='image/*' placeholder=' choose your image'></input>
-                    <input type="submit" value="Submit"></input>
+                        <input type="file" accept="image/*" placeholder=' choose your image' name="image" {...register("image")}></input>
+                    <input type="submit" value="Submit" ></input>
                 </form>
             </div>
         </div>
